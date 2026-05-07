b0493e4377fae4499937bf0a2b64a79e02bbfe85
Developer <dev@example.com>
2026-05-08 03:12:28 +0600

    Fix: Prioritize DB_URL for Aiven connection

--- backend/config/database.php
+++ backend/config/database.php
@@ -62,7 +62,6 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                (defined('PDO::MYSQL_ATTR_SSL_MODE') ? PDO::MYSQL_ATTR_SSL_MODE : 1016) => 1,
             ], fn($value) => !is_null($value)) : [],
         ],
 
